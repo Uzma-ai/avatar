321b53527a9da29f6e6827ba3d1438a206587dda
krunaldevtale <krunaldevtale79@gmail.com>
2024-11-22 11:16:56 +0530

    connecting the web socket

---
+++ app/chat/page.tsx
@@ -0,0 +1,13 @@
+// app/chat/page.tsx
+import ChatComponent from "@/components/ChatComponent";
+
+const ChatPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Chat with the Bot</h1>
+      <ChatComponent />
+    </div>
+  );
+};
+
+export default ChatPage;
